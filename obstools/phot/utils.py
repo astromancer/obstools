b0195bc7072533b6a869c22175afc962f5408597
astromancer <hannes@saao.ac.za>
2020-06-24 21:26:19 +0200

    remove defunct code

--- obstools/phot/utils.py
+++ obstools/phot/utils.py
@@ -2,27 +2,19 @@
 Miscellaneous utility functions
 """
 
-
 # std libs
 import logging
-import numbers
 import itertools as itt
 
 # third-party libs
 import numpy as np
 import more_itertools as mit
-from scipy.stats import binned_statistic_2d
 
 # local libs
-import motley
-from motley.table import Table
 from motley.progress import ProgressBar
 from recipes.logging import LoggingMixin
-from recipes.dict import AttrReadItem, ListLike
+from recipes.containers.dicts import AttrReadItem, ListLike
 from recipes.introspection.utils import get_module_name
-
-
-
 
 # module level logger
 logger = logging.getLogger(get_module_name(__file__))
@@ -93,268 +85,7 @@ def progressFactory(log=True, print_=True):
     return ProgressLogger, ProgressBar
 
 
-def table_coords(coo, ix_fit, ix_scale, ix_loc):
-    # TODO: maybe add flux estimate
-
-    # create table: coordinates
-    ocoo = np.array(coo[:, ::-1], dtype='O')
-    cootbl = Table(ocoo,
-                   col_headers=list('xy'),
-                   col_head_props=dict(bg='g'),
-                   row_headers=range(len(coo)),  # starts numbering from 0
-                   # number_rows=True,
-                   align='>',  # easier to read when right aligned
-                   )
-
-    # add colour indicators for tracking / fitting / scaling info
-    ms = 2
-    m = np.zeros((len(coo) + 1, 3), int)
-    # m[0] = 1, 2, 3
-
-    for i, ix in enumerate((ix_fit, ix_scale, ix_loc)):
-        m[ix, i] = i + 1
-
-    # flag stars
-    cols = 'gbm'
-    labels = ('fit|', 'scale|', 'loc|')
-    tags = np.empty(m.shape, dtype='U1')
-    tags[m != 0] = 'x'
-    # tags[:] = 'x' * ms
-
-    col_headers = motley.rainbow(labels, bg=cols)
-    tt = Table(tags, title='\n',  # title,   # title_props=None,
-               col_headers=col_headers,
-               frame=False, align='^',
-               col_borders='', cell_whitespace=0)
-    tt.colourise(m, fg=cols)
-    # ts = tt.add_colourbar(str(tt), ('fit|', 'scale|', 'loc|'))
-
-    # join tables
-    tbl = Table([[str(cootbl), str(tt)]], frame=False, col_borders='')
-    return tbl
-
-
-def table_cdist(sdist, window, _print=False):
-    # from scipy.spatial.distance import cdist
-    n = len(sdist)
-    # check for stars that are close together
-    # sdist = cdist(coo, coo)  # pixel distance between stars
-    # sdist[np.tril_indices(n)] = np.inf
-    #  since the distance matrix is symmetric, ignore lower half
-    # mask = (sdist == np.inf)
-
-    # create distance matrix as table, highlighting stars that are potentially
-    # too close together and may cause problems
-    bg = 'light green'
-    # tbldat = np.ma.array(sdist, mask=mask, copy=True)
-    tbl = Table(sdist,  # tbldat,
-                title='Distance matrix',
-                col_headers=range(n),
-                row_headers=range(n),
-                col_head_props=dict(bg=bg),
-                row_head_props=dict(bg=bg),
-                align='>')
-
-    if sdist.size > 1:
-        # Add colour as distance warnings
-        c = np.zeros_like(sdist)
-        c += (sdist < window / 2)
-        c += (sdist < window)
-        tbl.colourise(c, *' yr')
-        tbl.show_colourbar = False
-        tbl.flag_headers(c, bg=[bg] * 3, fg='wyr')
-
-    if _print and n > 1:
-        print(tbl)
-
-    return tbl  # , c
-
-
-def rand_median(cube, ncomb, subset, nchoose=None):
-    """
-    median combine `ncomb`` frames randomly from amongst `nchoose` in the interval
-    `subset`
-
-    Parameters
-    ----------
-    cube
-    ncomb
-    subset
-    nchoose
-
-    Returns
-    -------
-
-    """
-    if isinstance(subset, int):
-        subset = (0, subset)  # treat like a slice
-
-    i0, i1 = subset
-    if nchoose is None:  # if not given, select from entire subset
-        nchoose = i1 - i0
-
-    # get frame indices
-    nfirst = min(nchoose, i1 - i0)
-    ix = np.random.randint(i0, i0 + nfirst, ncomb)
-    # create median image for init
-    logger.info('Combining %i frames from amongst frames (%i->%i) for '
-                'reference image.', ncomb, i0, i0 + nfirst)
-    return np.median(cube[ix], 0)
-
-
-def duplicate_if_scalar(seq):
-    """
-
-    Parameters
-    ----------
-    seq : {number, array-like}
-
-    Returns
-    -------
-
-    """
-    # seq = np.atleast_1d(seq)
-    if np.size(seq) == 1:
-        seq = np.ravel([seq, seq])
-    if np.size(seq) != 2:
-        raise ValueError('Input should be of size 1 or 2')
-    return seq
-
-
-def shift_combine(images, offsets, stat='mean', extend=False):
-    """
-    Statistics on shifted image stack
-
-    Parameters
-    ----------
-    images
-    offsets
-    stat
-    extend
-
-    Returns
-    -------
-
-    """
-    # convert to (masked) array
-    images = np.asanyarray(images)
-    offsets = np.asanyarray(offsets)
-
-    # it can happen that `offsets` is masked (no stars in that frame)
-    if np.ma.is_masked(offsets):
-        # ignore images for which xy offsets are masked
-        bad = offsets.mask.any(1)
-        good = ~bad
-
-        logger.info(f'Removing {bad.sum()} images from stack due to null '
-                    f'detection')
-        images = images[good]
-        offsets = offsets[good]
-
-    # get pixel grid ignoring masked elements
-    shape = sy, sx = images.shape[1:]
-    grid = np.indices(shape)
-    gg = grid[:, None] - offsets[None, None].T
-    if np.ma.is_masked(images):
-        y, x = gg[:, ~images.mask]
-        sample = images.compressed()
-    else:
-        y, x = gg.reshape(2, -1)
-        sample = images.ravel()
-
-    # use maximal area coverage. returned image may be larger than input images
-    if extend:
-        y0, x0 = np.floor(offsets.min(0))
-        y1, x1 = np.ceil(offsets.max(0)) + shape + 1
-    else:
-        # returned image same size as original
-        y0 = x0 = 0
-        y1, x1 = np.add(shape, 1)
-
-    # compute statistic
-    yb, xb = np.ogrid[y0:y1, x0:x1]
-    bin_edges = (yb.ravel() - 0.5, xb.ravel() - 0.5)
-    results = binned_statistic_2d(y, x, sample, stat, bin_edges)
-    image = results.statistic
-    # mask nans (empty bins (pixels))
-
-    # note: avoid downstream warnings by replacing np.nan with zeros and masking
-    nans = np.isnan(image)
-    image[nans] = 0
-    return np.ma.MaskedArray(image, nans)
-
-
-class ImageSampler(object):
-    def __init__(self, data, sample_size=None):  # FIXME: sample_size --> subset
-        assert data.ndim == 3
-        self.data = data
-        self._axis = 0
-        self.sample_size = sample_size
-        # self.scaling_func = np.median
-        # self.scaling = False
-
-    def sample(self, n=None, subset=None):
-        """
-        Select a sample of `n` images randomly in the interval `subset`
-
-        Parameters
-        ----------
-        n
-        subset
-
-        Returns
-        -------
-
-        """
-
-        if n is None and self.sample_size is None:
-            raise ValueError('Please give sample size (or initialize this '
-                             'class with a sample size')
-
-        if subset is None:
-            subset = (0, len(self.data))
-
-        if isinstance(subset, numbers.Integral):
-            subset = (0, subset)  # treat like a slice
-
-        #
-        i0, i1 = subset
-
-        # get frame indices
-        # nfirst = min(n, i1 - i0)
-        ix = np.random.randint(i0, i1, n)
-        # create median image for init
-        logger.debug('Selecting %i frames from amongst frames (%i->%i) for '
-                     'sample image.', n, i0, i1)
-        return self.data[ix]
-
-    def max(self, n=None, subset=None):
-        return self.sample(n, subset).max(self._axis)
-
-    def mean(self, n=None, subset=None):
-        return self.sample(n, subset).mean(self._axis)
-
-    def std(self, n=None, subset=None):
-        return self.sample(n, subset).std(self._axis)
-
-    def median(self, n=None, subset=None):
-        return np.median(self.sample(n, subset), self._axis)
-
-    # def create_sample_image(self, interval, sample_size, statistic=np.median):
-    #     image = self.median(sample_size, interval)
-    #     # todo can add scaling into sampler ?
-    #     # scale = nd_sampler(image, np.median, 100)
-    #     # image_NORM = image / scale
-    #     mimage = np.ma.MaskedArray(image, BAD_PIXEL_MASK)
-    #     return mimage  # , scale
-
-
-class ImageSamplerHDUMixin(object):
-    def get_sample_image(self, size, interval=(None,), func='median',
-                         channel=...):
-        sampler = ImageSampler(self.data[:, channel], interval)
-        # TODO: handle callable func
-        return getattr(sampler, func)(size)
+# noinspection NonAsciiCharacters
 
 
 class Record(AttrReadItem, ListLike):
@@ -383,8 +114,9 @@ class LabelGroups(Record):
         return list(map(len, self.values()))
         # return [len(item) for item in self.values()]
 
-    # def inverse(self):
-    #     return {lbl: gid for gid, labels in self.items() for lbl in labels}
+    def inverse(self):
+        # fixme: if a label belongs to more than one group
+        return {lbl: gid for gid, labels in self.items() for lbl in labels}
 
 
 class LabelGroupsMixin(object):
