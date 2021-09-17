5dc4509ff8cb046f66ad841c343e0b5a362ea77c
astromancer <hannes@saao.ac.za>
2021-09-17 11:05:19 +0200

    photometry update and reordering bugfixes.

--- src/obstools/phot/core.py
+++ src/obstools/phot/core.py
@@ -6,24 +6,21 @@ import itertools as itt
 
 # third-party
 import numpy as np
+from loguru import logger
 
 # local
+from recipes import op
 from recipes.lists import split_like
 from recipes.iter import split_slices
-from recipes.logging import logging, get_module_logger
 
 # relative
+from .. import io
 from ..lc.ascii import write
 from ..campaign import PhotCampaign
-from .. import io
-
-from loguru import logger
 
 
 def ragged(hdu, seg, top=5, dilate=0, filename=None):
     # ragged aperture photometry (no tracking!!)
-    logger.info('Running ragged aperture photometry on hdu: %s',
-                hdu.file.name)
     seg = seg.dilate(dilate, copy=True)
     return seg.flux(hdu.calibrated, seg.labels[:top])
 
@@ -50,10 +47,13 @@ class PhotInterface:
         self.path = path
 
     def _runner(self, fun, day, segs, top=5, save=True, **kws):
-        # run photometry for multiple hdus of the same target and aggregate a light
-        # curve
+        # run photometry for multiple hdus of the same target and aggregate a
+        # light curve
 
         for hdu, seg in zip(day, segs):
+            logger.info('Running {:s} aperture photometry on hdu: {:s}',
+                        fun.__name__, hdu.file.name)
+
             flx, err = fun(hdu, seg, top, **kws)
 
             if save:
@@ -61,7 +61,6 @@ class PhotInterface:
                 folder = self.path / hdu.file.name.split('.', 1)[0]
                 if not folder.exists():
                     folder.mkdir()
-                    
                 filename = self.filename_template.format(hdu=hdu, fun=fun)
                 write(folder / filename,
                       hdu.timing.bjd, flx.T, err.T,
@@ -70,41 +69,40 @@ class PhotInterface:
             yield hdu.timing.bjd, flx, err
 
     def diff(self, fun, day, segs, top=5, save=True, **kws):
+        date = day[0].date
+        target = day[0].target
+        basepath = self.path / f'{date:d}-{fun.__name__}'
 
         sizes = day.attrs.nframes
         n = sum(sizes)
         labels = self.reg.attrs('seg.labels')[1:]
         nstars = max(map(max, labels)) - 1
         top = min(top, nstars)
-        
+
         logger.info('Light curves for {:d} sources will be extracted.', top)
 
-        target = 1
-        cmp = list(ftl.reduce(set.intersection, map(set, labels)) - {target})
-        cmp = np.array(cmp) - 1
+        target_label = 1
+        cmp = ftl.reduce(set.intersection, map(set, labels)) - {target_label}
+        cmp = np.array(list(cmp)) - 1
 
-        io.load_memmap(, (2, n, top))
-        
         times = np.empty(n)
-        results = np.ma.zeros((2, n, top)) # FIXME: memmap!!
+        # path = self.path / f'{basename}.dat'
+        results = io.load_memmap(basepath.with_suffix('.dat'), (2, n, top))
         sections = split_slices(itt.accumulate(sizes))
         datagen = self._runner(fun, day, segs, top, save, **kws)
         for section, seg, (t, flx, std) in zip(sections, segs, datagen):
             times[section] = t
 
             # rescale
-            idx = seg.labels[:top] - 1
+            idx = np.argsort(seg.labels[:top])
             results[:, section, idx] = \
                 (flx, std) / np.ma.median(flx[:, cmp], 1, keepdims=True)
 
-                
         if save:
             # save to plain text
-            hdu = day[0]
-            filename = self.path / f'{hdu.date:d}-{fun.__name__}.txt'
-            write(self.path / filename,
+            write(basepath.with_suffix('.txt'),
                   times, *np.swapaxes(results, 1, 2),
-                  obj_name=hdu.target)
+                  obj_name=target)
 
         return times, *results
 
@@ -113,12 +111,16 @@ class PhotInterface:
         assert not self.run.varies_by('target')
 
         ts = []
-        daily = self.run.group_by('date')
-        segs = split_like(self.reg.detections[1:], daily.values())
+        daily, indices = self.run.group_by('date', return_index=True)
+        order = op.itemgetter(*sum(indices.values(), []))(self.reg.order)
+        segs = op.itemgetter(*order)(self.reg.detections[1:])
+        segs = split_like(segs, daily.values())
         for (date, sub), segs in zip(daily.items(), segs):
-            if f'{date:d}' == '20131002':
-                ts.append(self.diff(fun, sub, segs, **kws))
-            
+            logger.info('Starting photometry for {!r:} on {!r:}',
+                            sub[0].target, date)
+
+            ts.append(self.diff(fun, sub, segs, **kws))
+
         return ts
 
     def ragged(self, top=5, dilate=0):
