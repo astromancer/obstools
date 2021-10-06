038a3aff22aa5e125ec0470f95911ec00204b3b0
astromancer <hannes@saao.ac.za>
2021-10-06 21:02:26 +0200

    bugfix + cleanup terminal imaging contour overlay

--- src/obstools/image/segmentation/display.py
+++ src/obstools/image/segmentation/display.py
@@ -1,8 +1,13 @@
+"""
+Display image cutouts in the console.
+"""
+
 # std
 import functools as ftl
 
 # third-party
 import numpy as np
+from loguru import logger
 
 # local
 import motley
@@ -40,32 +45,58 @@ LEFT_BORDER = '\N{LEFT ONE EIGHTH BLOCK}'
 # '⎵' Bottom square bracket 023B5
 
 
-def _add_vert(pixel, left, char, color):
-    csi, bg, fin, text, end = next(motley.ansi.parse(pixel))
+def _add_edge(pixel, left, char, color):
+    csi, nrs, fin, text, end = next(motley.ansi.parse(pixel))
     idx = int(left or -1)
-    parts = split(text, idx)
-    text = ''.join(parts[idx])
+    text = ''.join(split(text, idx)[idx])
     fg = motley.codes.get_code_str(color)
-    return ''.join((csi, fg, bg, fin, *(text, char)[::-idx], end))
+    # Apply color if this pixel doesn't already has an edge on the opposite side
+    # with a fg color!
+    if not nrs.startswith(fg):
+        nrs = fg + nrs
+    return ''.join((csi, nrs, fin, *(text, char)[::-idx], end))
 
 
-def right_vert(pixel, color=None):
-    return _add_vert(pixel, 0, RIGHT_BORDER, color)
+def add_right_edge(pixel, color=None):
+    return _add_edge(pixel, 0, RIGHT_BORDER, color)
 
 
-def left_vert(pixel, color):
-    return _add_vert(pixel, 1, LEFT_BORDER, color)
+def add_left_edge(pixel, color):
+    return _add_edge(pixel, 1, LEFT_BORDER, color)
+
+
+def _get_edge_drawing_funcs(color):
+    left = ftl.partial(add_left_edge, color=color)
+    right = ftl.partial(add_right_edge, color=color)
+    top = bottom = ftl.partial(motley.apply, fg=('_', color))
+    return [(left,   echo0, right),
+            (bottom, echo0, top)]
 
 
 def overlay(mask, pixels, color=None):
+    """
+    Overlay the contours from `mask` on the image `pixels`.
+
+    Parameters
+    ----------
+    mask : array-like
+        Boolean array of region to overlay the outline of.
+    pixels : array-like
+        Image pixels (strings).
+    color : str or None, optional
+        Colour of the contours, by default None.
+
+
+    Returns
+    -------
+    np.ndarray(dtype=str)
+        Pixels with edge characters added.
+    """
+    mask = np.asarray(mask)
     assert mask.shape == pixels.shape
 
     # edge drawing functions
-    left = ftl.partial(left_vert, color=color)
-    right = ftl.partial(right_vert, color=color)
-    top = bottom = ftl.partial(motley.apply, fg=('_', color))
-    edge_funcs = [(left,   echo0, right),
-                  (bottom, echo0, top)]
+    edge_funcs = _get_edge_drawing_funcs(color)
 
     # get boundary line
     (current, *_), boundary, _ = trace_boundary(mask)
@@ -75,36 +106,40 @@ def overlay(mask, pixels, color=None):
     out[:-1] = pixels
     # out = pixels.astype('O')
     # i = 0
-    for s in np.diff(boundary, axis=0):
-        w, = np.where(s)[0]
-        f = edge_funcs[w][s[w] + 1]
+    for step in np.diff(boundary, axis=0):
+        axis, = np.where(step)[0]
+        add_vertex = edge_funcs[axis][step[axis] + 1]
 
-        # logger.debug(s, w, s[w])
+        # logger.debug(step, axis, step[axis])
         offset = (0, 0)
-        if s[w] < 0:
-            offset = s
-        elif w == 0:
+        if step[axis] < 0:
+            offset = step
+        elif axis == 0:
             offset = (0, -1)
         ix = tuple(current + offset)
 
         # add edge character
-        out[ix] = f(out[ix])
+        out[ix] = add_vertex(out[ix])
+        logger.opt(lazy=True).debug('\n{}', lambda: motley.image.stack(out[::-1]))
         # update current pixel position
-        current += s
+        current += step
 
     return out.astype(str)
 
 
 class AnsiImage(motley.image.AnsiImage):
-    frame = True
+    # frame = True
     _top_row_added = False
 
     def overlay(self, mask, color=None):
         self.pixels = overlay(mask, self.pixels[::-1], color)[::-1].astype(str)
         self._top_row_added = True
 
-    def format(self, frame=frame):
+    def format(self, frame=None):
         pixels = self.pixels
+        if frame is None:
+            frame = self.frame
+
         if frame:
             pixels = motley.image.framed(self.pixels, not self._top_row_added)
 
