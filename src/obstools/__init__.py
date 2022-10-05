590ba441e00da7bd5efa10d44c96bc0108d525f7
astromancer <hannes@saao.ac.za>
2022-10-05 20:11:22 +0200

    FIX: HDU hash now accounts for live calibration images

--- src/obstools/__init__.py
+++ src/obstools/__init__.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 # local
-from recipes.caching import Reject
+from recipes.caching import Reject, hashers
 from recipes.dicts import AttrReadItem
 
 from loguru import logger
@@ -12,13 +12,25 @@ from loguru import logger
 logger.disable('obstools')
 
 
+def _cal_image_hasher(image):
+    if image is None:
+        return
+    return hashers.array(image)
+
+
 def _hdu_hasher(hdu):
-    # Cache on the hdu filename. hdu.file is NULL --> unsaved file, ignore
-    return str(hdu.file) if hdu.file else Reject(silent=False)
+    if hdu.file:
+        # Cache on the hdu filename.
+        return (str(hdu.file),
+                _cal_image_hasher(hdu.calibrated.dark),
+                _cal_image_hasher(hdu.calibrated.flat))
+
+    # hdu.file is NULL --> unsaved file, don't cache
+    return Reject(silent=False)
+
 
 # set hashing algorithm for HDU types
 # config(typed={HDUExtra: _hdu_hasher})
-
 
 # persistent caches for faster coordinate and image retrieval
 cachePath = _ = Path.home() / '.cache/obstools'  # NOTE only for linux!
