2ecfe20f7cb7215521344225f5e9dcfc487d9247
astromancer <hannes@saao.ac.za>
2024-02-05 15:10:45 +0200

    FIX: clip out-of-array indices

--- src/obstools/image/segments/neighbours.py
+++ src/obstools/image/segments/neighbours.py
@@ -1,17 +1,26 @@
 
+# third-party
 import numpy as np
 
+# local
+from recipes.functionals import echo
 
+
+# ---------------------------------------------------------------------------- #
 _neighbours = {
     4: np.array([(-1, 0), (0, -1), (0, 1), (1, 0)]).T,
     8: np.array([(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]).T
 }
 
 
+# ---------------------------------------------------------------------------- #
 def get_neighbour_index(pos, size=1, connectivity=8):
     return np.round(pos).astype(int)[..., np.newaxis] + _neighbours[connectivity]
 
 
-def get_neighbours(a, pos, size=1, connectivity=8):
-    n = get_neighbour_index(pos, size, connectivity)
-    return a[tuple(np.moveaxis(n, 1, 0))]
+def get_neighbours(a, pos, size=1, connectivity=8, func=echo):
+    indices = get_neighbour_index(pos, size, connectivity)
+    bad = (-1 > indices) | indices >= np.atleast_3d(a.shape)
+    for i, x in zip(indices, bad):
+        yield func(a[tuple(i[:, np.logical_not(x.any(0))])])
+        # return a[tuple(np.moveaxis(indices, 1, 0))]
